bbea194fd7fa90506d1065e4a2aa8ba2f17a607b
Augusto Fleith Comitti <gutocomitti@gmail.com>
2022-11-30 11:06:29 -0300

    Creating VideoList component and passing the videos from the API to it

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
-  <div id="app">
+  <div>
     <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList :videos="videos"></VideoList>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 import SearchBar from "./components/SearchBar";
-// https://developers.google.com/youtube/v3/docs/search
+import VideoList from './components/VideoList';
+
+// Google API documentation: https://developers.google.com/youtube/v3/docs/search
 const API_KEY = 'AIzaSyDR0SPom8u7YGlMSNSW7sT2ngpDJ4hTiJc';
 const YOUTUBE_API_SEARCH_ENDPOINT = 'https://www.googleapis.com/youtube/v3/search';
 
 export default {
   name: "App",
+  data() {
+    return {
+      videos: []
+    }
+  },
   components: {
     SearchBar,
+    VideoList
   },
   methods: {
     onTermChange(term) {
@@ -28,7 +37,8 @@ export default {
       }).then(this.handleYoutubeResponse);
     },
     handleYoutubeResponse(res) {
-      console.log(res);
+      this.videos = res.data.items;
+      console.log(this.videos);
     }
   }
 };
