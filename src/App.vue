f92723dde2c973a38a5a38997f8952a5ad664d77
Augusto Fleith Comitti <gutocomitti@gmail.com>
2022-11-30 09:42:55 -0300

    Implementing youtube search

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
   <div id="app">
-    <SearchBar></SearchBar>
+    <SearchBar @termChange="onTermChange"></SearchBar>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import SearchBar from "./components/SearchBar";
+// https://developers.google.com/youtube/v3/docs/search
+const API_KEY = 'AIzaSyDR0SPom8u7YGlMSNSW7sT2ngpDJ4hTiJc';
+const YOUTUBE_API_SEARCH_ENDPOINT = 'https://www.googleapis.com/youtube/v3/search';
+
 export default {
   name: "App",
   components: {
     SearchBar,
   },
+  methods: {
+    onTermChange(term) {
+      axios.get(YOUTUBE_API_SEARCH_ENDPOINT, {
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part: 'snippet',
+          q: term
+        }
+      }).then(this.handleYoutubeResponse);
+    },
+    handleYoutubeResponse(res) {
+      console.log(res);
+    }
+  }
 };
 </script>
 
