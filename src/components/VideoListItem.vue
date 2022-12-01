ff2c15dcb9082665782fec640fcd8d05b60adc62
Augusto Fleith Comitti <gutocomitti@gmail.com>
2022-12-01 10:03:06 -0300

    Adding and styling the thumbnail

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,13 +1,32 @@
 <template>
-    <li class="list-group-item">
-        {{ video.snippet.title }}
+    <li class="list-group-item media">
+        <img :src="thumbnail.url" class="mr-3">
+        <div class="media-body">
+            {{ video.snippet.title }}
+        </div>
     </li>
 </template>
 <script>
 export default {
     name: 'VideoListItem',
-    props: ['video']
+    props: ['video'],
+    computed: {
+        thumbnail(){
+            return this.video.snippet.thumbnails.default;
+        }
+    }
 
 }
 
 </script>
+
+<style scoped>
+    li {
+        display: flex;
+        cursor: pointer;
+    }
+
+    li:hover {
+        background-color: #eee;
+    }
+</style>
